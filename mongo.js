d7a82a83c3dd34eec9efdb17cead5b3f1c6d49ec
John Pape <jpapejr@icloud.com>
2019-06-28 14:24:28 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -4,7 +4,7 @@
 //db.resourceStats.find().forEach(printjson)
 
 db.clusters.deleteOne( { "cluster_id": cluster } )
-db.resources.deleteOne( { "cluster_id": cluster } )
+db.resources.deleteMany( { "cluster_id": cluster } )
 var clusterCount = db.clusters.count()
 var resourceCount = db.resources.count()
 db.resourceStats.updateOne({}, { $set: { "clusterCount": clusterCount } })
